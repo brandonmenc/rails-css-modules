9245fdc24191c9c8733ef4d67e2e2c0c6f1e16be
Brandon Menc <brandonmenc@icloud.com>
2019-09-08 21:45:55 -0500

    Example view and styles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @specs = [
+      OpenStruct.new(title: "Storage", value: "100 gazillion bytes"),
+      OpenStruct.new(title: "Speed", value: "1000 bits per minute")
+    ]
   end
 end
