03a9c7c6d72a9e436dd3607a608e9b67e3427c27
Brandon Menc <brandonmenc@icloud.com>
2019-09-08 21:45:01 -0500

    Add view helper for css module classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def c(selector)
+    # For example, will convert "welcome/specs/_specs" to "welcome-specs"
+    dashed_path = Pathname(@virtual_path).each_filename.to_a[0..-2].join('-')
+
+    "app-javascript-styles-#{dashed_path}-module__#{selector}"
+  end
 end
