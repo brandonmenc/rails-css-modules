70d80b331ae491c041e25cd89bf863220785dd7c
Brandon Menc <brandonmenc@icloud.com>
2019-09-08 21:43:03 -0500

    Set the css class naming scheme

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
+const merge = require('webpack-merge')
+
+const myCssLoaderOptions = {
+    modules: true,
+    sourceMap: true,
+    // https://github.com/webpack/loader-utils#interpolatename
+    localIdentName: '[path][name]__[local]'
+}
+
+const CSSLoader = environment.loaders.get('moduleSass').use.find(el => el.loader === 'css-loader')
+
+CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
 
 module.exports = environment
